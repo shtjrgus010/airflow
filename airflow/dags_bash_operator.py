b53fe5d93738f32ce0e7c816cf9d963030679735
shtjrgus010 <shtjrgus010@naver.com>
2024-06-22 22:45:57 +0900

    bashOperator

---
+++ airflow/dags_bash_operator.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import datetime
+
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    # 웹 페이지에 보이는 이름
+    dag_id="example_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2024, 6, 22, tz="Asia/Seoul"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    # 웹에 보이는 tag들
+    tags=["example", "example2"],
+    params={"example_key": "example_value"},
+) as dag:
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+
+    run_this_last = EmptyOperator(
+        task_id="run_this_last",
+    )
+
+    bash_t1 >> bash_t2

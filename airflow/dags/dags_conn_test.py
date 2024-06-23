2f1cd7914fe7d2de19e1c61edadb4859c776d1c4
shtjrgus010 <shtjrgus010@naver.com>
2024-06-23 15:26:40 +0900

    dags_bash_select_fruit.py

--- airflow/dags/dags_conn_test.py
+++ airflow/dags/dags_conn_test.py
@@ -7,7 +7,7 @@ from airflow.operators.empty import EmptyOperator
 with DAG(
     dag_id="dags_conn_test",
     schedule=None,
-    start_date=pendulum.datetime(2023, 3, 1, tz="Aisa/Seoul"),
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
 

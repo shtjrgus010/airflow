deb6b98e9570c6c5d99f04c8ee6023288ac0380d
shtjrgus010 <shtjrgus010@naver.com>
2024-06-30 15:50:46 +0900

    fix

--- airflow/dags/dags_python_template.py
+++ airflow/dags/dags_python_template.py
@@ -19,7 +19,7 @@ with DAG(
         task_id="python_t1",
         python_callable=python_function1,
         op_kwargs={
-            "start_date": "{{data__interval_start | ds}}",
+            "start_date": "{{data_interval_start | ds}}",
             "end_date": "{{data_interval_end | ds }}",
         },
     )
